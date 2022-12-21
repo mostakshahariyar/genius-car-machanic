dc2fb724c21b8b6b2136ef52ff8b72b91ea9e222
Mostak Shahariyar <mostakshahariyar18@gmail.com>
2022-12-21 23:32:07 +0600

    navbar-add and responsive

--- src/Pages/Navbar/Navbar.js
+++ src/Pages/Navbar/Navbar.js
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
         return (
-                <nav class="navbar navbar-collapse  navbar-dark bg-dark navbar-expand-lg sticky-top">
+                <nav class="navbar navbar-collapse  navbar-dark bg-dark navbar-expand-lg ">
                         <div class="container-fluid">
                                 <a class="navbar-brand" href="#">Geniyas Car Mechanis</a>
                                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                                         <span class="navbar-toggler-icon"></span>
                                 </button>
-                                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                <div class="collapse navbar-collapse navbar-toggler" id="navbarSupportedContent">
                                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                                                 <li class="nav-item">
                                                         <Link class="nav-link" aria-current="page" to={"/home"}>Home</Link>
