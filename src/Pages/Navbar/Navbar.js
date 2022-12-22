50e43d28c96d513b1c08515578bec6190a4ee398
Mostak Shahariyar <mostakshahariyar18@gmail.com>
2022-12-22 23:31:56 +0600

    firebase and netlify page refresh error solved

--- src/Pages/Navbar/Navbar.js
+++ src/Pages/Navbar/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
         return (
                 <nav class="navbar navbar-collapse navbar-dark bg-dark navbar-expand-lg sticky-top">
                         <div class="container">
-                                <Link class="navbar-brand fw-bold" to="/">Geniyas Car Mechanis</Link>
+                        <Link class="navbar-brand fw-bold" to="/">Geniyas Car Mechanis</Link>
                                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                                         <span class="navbar-toggler-icon"></span>
                                 </button>
